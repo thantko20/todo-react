7be29cc9ae25717ec6f380fccf299cb08641a26c
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-06 20:24:59 +0630

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Outlet } from 'react-router-dom';
 
-function App() {
+const Layout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
-}
+};
+
+const Page1 = () => {
+  return <h1>This is page 1</h1>;
+};
+
+const Page2 = () => {
+  return <h1>This is page 2</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Page1 />} />
+        <Route path='/page2' element={<Page2 />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBAapIB8_3Rhpisc-oA5BuVN24qc5Gd1w',
+  authDomain: 'todos-list-react-ts.firebaseapp.com',
+  projectId: 'todos-list-react-ts',
+  storageBucket: 'todos-list-react-ts.appspot.com',
+  messagingSenderId: '1076421844184',
+  appId: '1:1076421844184:web:d005faf9fe9da317da8bad',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };

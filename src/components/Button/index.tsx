c7d24473a954a417d695c6de781e64e856bc7e94
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-10 13:50:25 +0630

    Add reusable button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,16 +5,29 @@ type OnClickFn = () => void;
 interface Props {
   children?: React.ReactNode;
   onClick: OnClickFn;
+  variant?: 'primary' | 'secondary';
 }
 
-const Button = ({ children, onClick }: Props) => {
+const Button = ({ children, onClick, variant = 'primary' }: Props) => {
   return (
-    <button
-      onClick={onClick}
-      className='px-4 py-2 bg-indigo-500 text-indigo-50 text-sm font-bold rounded hover:bg-indigo-600'
-    >
-      {children}
-    </button>
+    <>
+      {variant === 'primary' && (
+        <button
+          onClick={onClick}
+          className='px-8 py-3 font-semibold bg-indigo-500 text-indigo-50 rounded tracking-wider transition-colors hover:bg-indigo-600'
+        >
+          {children}
+        </button>
+      )}
+      {variant === 'secondary' && (
+        <button
+          onClick={onClick}
+          className='px-8 py-3 bg-indigo-50 font-semibold text-indigo-500 rounded tracking-wider transition-colors hover:bg-indigo-100'
+        >
+          {children}
+        </button>
+      )}
+    </>
   );
 };
 

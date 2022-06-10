b3070da0577f0cb8016a9cb4f00e7f6002ca3389
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-10 16:50:51 +0630

    Add methods for fetching data

--- src/firebase/db.utils.js
+++ src/firebase/db.utils.js
@@ -1,5 +1,6 @@
 import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from './firebase';
+import uniqid from 'uniqid';
 
 const saveUser = async (userInfo) => {
   try {
@@ -19,4 +20,36 @@ const saveUser = async (userInfo) => {
   }
 };
 
-export { saveUser };
+const getProjects = async (userId) => {
+  const projectsRef = collection(db, 'projects');
+  const q = query(projectsRef, where('ownerId', '==', userId));
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) {
+    return [];
+  }
+
+  const tempData = [];
+  snapshot.forEach((item) => tempData.push(item.data()));
+
+  console.log(tempData);
+
+  return tempData;
+};
+
+const addProject = async (projectName, userId) => {
+  try {
+    const projectsRef = collection(db, 'projects');
+
+    await addDoc(projectsRef, {
+      name: projectName,
+      todos: [],
+      id: uniqid(),
+      ownerId: userId,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { saveUser, getProjects, addProject };
